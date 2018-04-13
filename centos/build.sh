f5533fc188cbefd3b6bf6db8460de774068ca995
wangxiang <wx1983@gmail.com>
2018-04-13 11:58:14 +0800

    add tag 7.3 for centos

--- centos/build.sh
+++ centos/build.sh
@@ -2,9 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker build -t "szewec/centos:7.3.1611" -t "szewec/centos:latest" --force-rm "$DIR/7.3.1611"
+docker build -t "szewec/centos:7.3.1611" -t "szewec/centos:7.3" -t "szewec/centos:latest" --force-rm "$DIR/7.3.1611"
 
 if [[ "$PUSH_IMAGES" = "true" ]]; then
   docker push "szewec/centos:7.3.1611"
+  docker push "szewec/centos:7.3"
   docker push "szewec/centos:latest"
 fi
