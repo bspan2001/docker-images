627246a19c14c61561df6e1933d63e77950a0289
wangxiang <wx1983@gmail.com>
2018-04-13 13:21:47 +0800

    disable build java with libreoffice or openoffice

--- java/build.sh
+++ java/build.sh
@@ -3,16 +3,20 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IMAGE_NAME=szewec/${DIR##*/}
 
+docker pull "${IMAGE_NAME}:openjdk-8" || true
+# docker pull "${IMAGE_NAME}:openjdk-8-libreoffice5.3" || true
+# docker pull "${IMAGE_NAME}:openjdk-8-openoffice4" || true
+
 docker build -t "${IMAGE_NAME}:openjdk-8" -t "${IMAGE_NAME}:8" -t "${IMAGE_NAME}:latest" --force-rm "${DIR}/openjdk-8"
-docker build -t "${IMAGE_NAME}:openjdk-8-libreoffice5.3" -t "${IMAGE_NAME}:8-libreoffice5.3" --force-rm "${DIR}/openjdk-8-libreoffice5.3"
-docker build -t "${IMAGE_NAME}:openjdk-8-openoffice4" -t "${IMAGE_NAME}:8-openoffice4" --force-rm "${DIR}/openjdk-8-openoffice4"
+# docker build -t "${IMAGE_NAME}:openjdk-8-libreoffice5.3" -t "${IMAGE_NAME}:8-libreoffice5.3" --force-rm "${DIR}/openjdk-8-libreoffice5.3"
+# docker build -t "${IMAGE_NAME}:openjdk-8-openoffice4" -t "${IMAGE_NAME}:8-openoffice4" --force-rm "${DIR}/openjdk-8-openoffice4"
 
 if [[ "$PUSH_IMAGES" = "true" ]]; then
   docker push "${IMAGE_NAME}:openjdk-8"
   docker push "${IMAGE_NAME}:8"
   docker push "${IMAGE_NAME}:latest"
-  docker push "${IMAGE_NAME}:openjdk-8-libreoffice5.3"
-  docker push "${IMAGE_NAME}:8-libreoffice5.3"
-  docker push "${IMAGE_NAME}:openjdk-8-openoffice4"
-  docker push "${IMAGE_NAME}:8-openoffice4"
+  # docker push "${IMAGE_NAME}:openjdk-8-libreoffice5.3"
+  # docker push "${IMAGE_NAME}:8-libreoffice5.3"
+  # docker push "${IMAGE_NAME}:openjdk-8-openoffice4"
+  # docker push "${IMAGE_NAME}:8-openoffice4"
 fi
