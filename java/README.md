a73fcb5e43892cbc90abb37cc3b2e426f7dc6c68
wangxiang <wx1983@gmail.com>
2018-04-17 17:45:51 +0800

    add java:7

---
+++ java/README.md
@@ -0,0 +1,4 @@
+# supported tags
+
+* [`openjdk-7`, `7` (java/openjdk-7/Dockerfile)](https://github.com/szewec/docker-images/blob/master/java/openjdk-7/Dockerfile)
+* [`openjdk-8`, `8`, `latest` (java/openjdk-7/Dockerfile)](https://github.com/szewec/docker-images/blob/master/java/openjdk-8/Dockerfile)
--- java/build.sh
+++ java/build.sh
@@ -8,6 +8,8 @@ source $DIR/../lib.sh
 
 if [[ "$PULL_IMAGES" = "true" ]]; then
   # pull all tags of the docker image
+  docker pull "${IMAGE_NAME}:openjdk-7" || true
+  docker pull "${IMAGE_NAME}:7" || true
   docker pull "${IMAGE_NAME}:openjdk-8" || true
   docker pull "${IMAGE_NAME}:8" || true
   docker pull "${IMAGE_NAME}:latest" || true
@@ -19,23 +21,15 @@ fi
 
 if [[ "$PULL_IMAGES_ONLY" != "true" ]]; then
   # build docker image with multiple tags
-  docker build -t "${IMAGE_NAME}:openjdk-8" \
-               -t "${IMAGE_NAME}:8" \
-               -t "${IMAGE_NAME}:latest" \
-               --force-rm \
-               "${DIR}/openjdk-8"
-  # docker build -t "${IMAGE_NAME}:openjdk-8-libreoffice5.3" \
-  #              -t "${IMAGE_NAME}:8-libreoffice5.3" \
-  #              --force-rm \
-  #              "${DIR}/openjdk-8-libreoffice5.3"
-  # docker build -t "${IMAGE_NAME}:openjdk-8-openoffice4" \
-  #              -t "${IMAGE_NAME}:8-openoffice4" \
-  #              --force-rm \
-  #              "${DIR}/openjdk-8-openoffice4"
+  docker build -t "${IMAGE_NAME}:openjdk-7" -t "${IMAGE_NAME}:7" --force-rm "${DIR}/openjdk-7"
+  docker build -t "${IMAGE_NAME}:openjdk-8" -t "${IMAGE_NAME}:8" -t "${IMAGE_NAME}:latest" --force-rm "${DIR}/openjdk-8"
+  # docker build -t "${IMAGE_NAME}:openjdk-8-libreoffice5.3" -t "${IMAGE_NAME}:8-libreoffice5.3" --force-rm "${DIR}/openjdk-8-libreoffice5.3"
+  # docker build -t "${IMAGE_NAME}:openjdk-8-openoffice4" -t "${IMAGE_NAME}:8-openoffice4" --force-rm "${DIR}/openjdk-8-openoffice4"
 
   if [[ "$ENABLE_CACHE" = "true" ]]; then
     # save docker image to cache dir
     mkdir -p ${CACHE_DIR}/szewec
+    docker save -o "${CACHE_DIR}/${IMAGE_NAME}-openjdk-7.tar" "${IMAGE_NAME}:openjdk-7"
     docker save -o "${CACHE_DIR}/${IMAGE_NAME}-openjdk-8.tar" "${IMAGE_NAME}:openjdk-8"
     # docker save -o "${CACHE_DIR}/${IMAGE_NAME}-openjdk-8-libreoffice5.3.tar" "${IMAGE_NAME}:openjdk-8-libreoffice5.3"
     # docker save -o "${CACHE_DIR}/${IMAGE_NAME}-openjdk-8-openoffice4.tar" "${IMAGE_NAME}:openjdk-8-openoffice4"
@@ -44,6 +38,8 @@ fi
 
 if [[ "$PUSH_IMAGES" = "true" ]]; then
   # push all tags of the docker image
+  docker push "${IMAGE_NAME}:openjdk-7"
+  docker push "${IMAGE_NAME}:7"
   docker push "${IMAGE_NAME}:openjdk-8"
   docker push "${IMAGE_NAME}:8"
   docker push "${IMAGE_NAME}:latest"
