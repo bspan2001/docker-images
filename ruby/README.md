3b938a0d41b8f29e587d568cf20c1dedec793e6a
wangxiang <wx1983@gmail.com>
2018-04-17 17:44:50 +0800

    add ruby 2.2, 2.3, 2.4

---
+++ ruby/README.md
@@ -0,0 +1,5 @@
+# supported tags
+
+* [`2.2` (ruby/2.2/Dockerfile)](https://github.com/szewec/docker-images/blob/master/ruby/2.2/Dockerfile)
+* [`2.3`, (ruby/2.3/Dockerfile)](https://github.com/szewec/docker-images/blob/master/ruby/2.3/Dockerfile)
+* [`2.4`, `latest` (ruby/2.4/Dockerfile)](https://github.com/szewec/docker-images/blob/master/ruby/2.4/Dockerfile)
--- ruby/build.sh
+++ ruby/build.sh
@@ -8,29 +8,31 @@ source $DIR/../lib.sh
 
 if [[ "$PULL_IMAGES" = "true" ]]; then
   # pull all tags of the docker image
-  docker pull "${IMAGE_NAME}:2.0.0" || true
-  docker pull "${IMAGE_NAME}:2.0" || true
+  docker pull "${IMAGE_NAME}:2.2" || true
+  docker pull "${IMAGE_NAME}:2.3" || true
+  docker pull "${IMAGE_NAME}:2.4" || true
   docker pull "${IMAGE_NAME}:latest" || true
 fi
 
 if [[ "$PULL_IMAGES_ONLY" != "true" ]]; then
   # build docker image with multiple tags
-  docker build -t "${IMAGE_NAME}:2.0.0" \
-               -t "${IMAGE_NAME}:2.0" \
-               -t "${IMAGE_NAME}:latest" \
-               --force-rm \
-               "${DIR}/2.0.0"
+  docker build -t "${IMAGE_NAME}:2.2" --force-rm "${DIR}/2.2"
+  docker build -t "${IMAGE_NAME}:2.3" --force-rm "${DIR}/2.3"
+  docker build -t "${IMAGE_NAME}:2.4" -t "${IMAGE_NAME}:latest" --force-rm "${DIR}/2.4"
 
   if [[ "$ENABLE_CACHE" = "true" ]]; then
     # save docker image to cache dir
     mkdir -p ${CACHE_DIR}/szewec
-    docker save -o "${CACHE_DIR}/${IMAGE_NAME}-2.0.0.tar" "${IMAGE_NAME}:2.0.0"
+    docker save -o "${CACHE_DIR}/${IMAGE_NAME}-2.2.tar" "${IMAGE_NAME}:2.2"
+    docker save -o "${CACHE_DIR}/${IMAGE_NAME}-2.3.tar" "${IMAGE_NAME}:2.3"
+    docker save -o "${CACHE_DIR}/${IMAGE_NAME}-2.4.tar" "${IMAGE_NAME}:2.4"
   fi
 fi
 
 if [[ "$PUSH_IMAGES" = "true" ]]; then
   # push all tags of the docker image
-  docker push "${IMAGE_NAME}:2.0.0"
-  docker push "${IMAGE_NAME}:2.0"
+  docker push "${IMAGE_NAME}:2.2"
+  docker push "${IMAGE_NAME}:2.3"
+  docker push "${IMAGE_NAME}:2.4"
   docker push "${IMAGE_NAME}:latest"
 fi
