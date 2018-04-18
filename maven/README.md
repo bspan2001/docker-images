6207904beb7cde122adaeed2acaac01b64253142
wangxiang <wx1983@gmail.com>
2018-04-18 09:41:49 +0800

    add maven image

---
+++ maven/README.md
@@ -0,0 +1,3 @@
+# supported tags
+
+* [`3.5-jdk-8`, `3.5`, `latest` (maven/3.5-jdk-8/Dockerfile)](https://github.com/szewec/docker-images/blob/master/maven/3.5-jdk-8/Dockerfile)
---
+++ maven/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -ex
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+IMAGE_NAME=szewec/${DIR##*/}
+CACHE_DIR=${CACHE_DIR:-${DIR}/.cache}
+
+source $DIR/../lib.sh
+
+if [[ "$PULL_IMAGES" = "true" ]]; then
+  # pull all tags of the docker image
+  docker pull "${IMAGE_NAME}:3.5-jdk-8" || true
+  docker pull "${IMAGE_NAME}:3.5" || true
+  docker pull "${IMAGE_NAME}:latest" || true
+  # docker pull "${IMAGE_NAME}:3.5-jdk-8-libreoffice5.3" || true
+  # docker pull "${IMAGE_NAME}:3.5-jdk-8-openoffice4" || true
+fi
+
+if [[ "$PULL_IMAGES_ONLY" != "true" ]]; then
+  # build docker image with multiple tags
+  docker build -t "${IMAGE_NAME}:3.5-jdk-8" -t "${IMAGE_NAME}:3.5" -t "${IMAGE_NAME}:latest" --force-rm "${DIR}/3.5-jdk-8"
+  # docker build -t "${IMAGE_NAME}:3.5-jdk-8-libreoffice5.3" --force-rm "${DIR}/3.5-jdk-8-libreoffice5.3"
+  # docker build -t "${IMAGE_NAME}:3.5-jdk-8-openoffice4" --force-rm "${DIR}/3.5-jdk-8-openoffice4"
+
+  if [[ "$ENABLE_CACHE" = "true" ]]; then
+    # save docker image to cache dir
+    mkdir -p ${CACHE_DIR}/szewec
+    docker save -o "${CACHE_DIR}/${IMAGE_NAME}-3.5-jdk-8.tar" "${IMAGE_NAME}:3.5-jdk-8"
+    # docker save -o "${CACHE_DIR}/${IMAGE_NAME}-3.5-jdk-8-libreoffice5.3.tar" "${IMAGE_NAME}:3.5-jdk-8-libreoffice5.3"
+    # docker save -o "${CACHE_DIR}/${IMAGE_NAME}-3.5-jdk-8-openoffice4.tar" "${IMAGE_NAME}:3.5-jdk-8-openoffice4"
+  fi
+fi
+
+if [[ "$PUSH_IMAGES" = "true" ]]; then
+  # push all tags of the docker image
+  docker push "${IMAGE_NAME}:3.5-jdk-8"
+  docker push "${IMAGE_NAME}:3.5"
+  docker push "${IMAGE_NAME}:latest"
+  # docker push "${IMAGE_NAME}:3.5-jdk-8-libreoffice5.3"
+  # docker push "${IMAGE_NAME}:3.5-jdk-8-openoffice4"
+fi
