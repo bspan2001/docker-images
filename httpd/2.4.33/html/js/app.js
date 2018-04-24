ba199f1d6602a0e0362740e732e171312d00d217
wangxiang <wx1983@gmail.com>
2018-04-24 20:10:25 +0800

    add httpd

---
+++ httpd/2.4.33/html/js/app.js
@@ -0,0 +1,8 @@
+// 百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3617eb2d825629e82dbae515f118d3b8";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
